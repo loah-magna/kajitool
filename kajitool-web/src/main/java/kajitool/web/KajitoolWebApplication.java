3ee456dfee4991758997ec54a0166afeac2c9ee2
takai-yuki <loahpsoarks@gmail.com>
2020-04-08 12:55:59 +0900

    no message

---
+++ kajitool-web/src/main/java/kajitool/web/KajitoolWebApplication.java
@@ -0,0 +1,13 @@
+package kajitool.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KajitoolWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KajitoolWebApplication.class, args);
+	}
+
+}
